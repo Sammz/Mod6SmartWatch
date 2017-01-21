c9ca26e38a12c27a961bf21319471df045d30d7e
s1672509 <s.j.witt@student.utwente.nl>
2017-01-21 18:55:07 +0100

    ambient mode werkt

--- wear/src/main/java/com/example/sam/mod6good/MainActivity.java
+++ wear/src/main/java/com/example/sam/mod6good/MainActivity.java
@@ -1,24 +1,30 @@
 package com.example.sam.mod6good;
 
-import android.app.Activity;
+
 import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 
-public class MainActivity extends Activity implements View.OnClickListener, SensorEventListener {
+
+
+public class MainActivity extends WearableActivity implements View.OnClickListener, SensorEventListener {
 
 
     private static final String TAG = "MainActivity";
     private TextView mTextViewHeart;
     Button button2;
+    private long currentTime;
+
+// adb -e logcat MainActivity:i *:S > mod6log.txt
 
 
 
@@ -26,8 +32,9 @@ public class MainActivity extends Activity implements View.OnClickListener, Sens
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.round_activity_main);
+        setAmbientEnabled();
 
+        setContentView(R.layout.round_activity_main);
 
         button2 = (Button) findViewById(R.id.ButtonOptions);
         button2.setOnClickListener(this);
@@ -35,12 +42,11 @@ public class MainActivity extends Activity implements View.OnClickListener, Sens
         SensorManager mSensorManager = ((SensorManager)getSystemService(SENSOR_SERVICE));
         Sensor mHeartRateSensor = mSensorManager.getDefaultSensor(21);
         mSensorManager.registerListener(this, mHeartRateSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        Log.i(TAG, "LISTENER REGISTERED.");
+        Log.i("", "LISTENER REGISTERED.");
         mTextViewHeart.setText("Stressmeter");
 
-        for (Sensor sensor : mSensorManager.getSensorList(Sensor.TYPE_ALL)) {
-            Log.e("HB Service", sensor.getName() + ": " + sensor.getType());
-        }
+        currentTime = System.currentTimeMillis();
+        Log.d("timestamp: ", ""+(currentTime/1000000));
        mSensorManager.registerListener(this, mHeartRateSensor, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
@@ -60,9 +66,11 @@ public class MainActivity extends Activity implements View.OnClickListener, Sens
     @Override
     public void onSensorChanged(SensorEvent event) {
         if (event.sensor.getType() == Sensor.TYPE_HEART_RATE) {
-            String msg = "" + (int)event.values[0];
+            String msg = "" + (int)event.values[0] + " ";
             mTextViewHeart.setText(msg);
-            Log.d(TAG, msg);
+            String time = "" + (System.currentTimeMillis() - currentTime)/1000;
+            Log.i(TAG, msg + " " + time);
+
         }
         else
             Log.d(TAG, "Unknown sensor type");
